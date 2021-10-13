ef033c4e12b071844d85fdc96a8570d639c89a88
KC Kern <kc@kckern.com>
2021-10-14 06:28:08 +0900

    Service-level functions

---
+++ src/api/Telegram.js
@@ -0,0 +1,53 @@
+
+
+const axios = require('axios');
+const telegramBotToken = "2037460170:AAE0rfJLXFxWylxHBGRlJgtMwe_d27nldIE";
+const SendbirdDesk = require('./SendbirdDesk');
+
+class Telegram {
+    constructor(app_id, token) {
+        this.app_id = app_id;
+        this.token = token;
+    }
+
+    processWebhook(payload, res, SendbirdDesk) {
+
+        const user_id = payload.message.from.id;
+        const chat_id = payload.message.chat.id;
+        const sendbird_id = `telegram_${chat_id}_${user_id}`;
+        const message = payload.message.text;
+        const nickname = payload.message.from.first_name+" "+payload.message.from.last_name;
+        return SendbirdDesk.processMessage(sendbird_id,message,nickname,{"telegram":chat_id.toString()}).then(result=>res.send(result))
+    }
+
+
+    sendMessage(chat_id, message) {
+
+        console.log("TELEGRAM sendMessage",{chat_id,message});
+        var authOptions = {
+            method: 'POST',
+            url: `https://api.telegram.org/bot${telegramBotToken}/sendMessage`,
+            data: {
+                "chat_id": chat_id,
+                "text": message
+            },
+            json: true
+        };
+        return axios(authOptions).then(res => {
+            console.log("TELEGRAM MESSAGE SENT!");
+            return "TELEGRAM MESSAGE SENT! ";
+        }).catch(error => {
+            console.log("TELEGRAM MESSAGE ERROR",error);
+            return false
+        })
+
+    }
+
+
+
+}
+
+
+var telegram = new Telegram();
+
+module.exports = telegram
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,24 @@
 const router = require('express').Router();
-const { closeTicket } = require('./api/closeTicket');
+
 // IMPORT ROUTES
+const SendbirdDesk = require('./api/SendbirdDesk');
+const SendbirdChat = require('./api/SendbirdChat');
+const Telegram = require('./api/Telegram');
+const Twilio = require('./api/Twilio');
+const GoogleMyBusiness = require('./api/GoogleMyBusiness');
+
+
 const ticketRouting = require('./api/ticketRouting');
-// Define routes
+const { closeTicket } = require('./api/closeTicket');
+
+// Webhook Endpoints
+router.post('/desk',        (req, res) => SendbirdDesk.processWebhook(req.body, res));
+router.post('/chat',        (req, res) => SendbirdChat.processWebhook(req.body, res));
+router.post('/telegram',    (req, res) => Telegram.processWebhook(req.body, res, SendbirdDesk));
+router.post('/twilio',      (req, res) => Twilio.processWebhook(req.body, res));
+router.post('/gmb',         (req, res) => GoogleMyBusiness.processWebhook(req.body, res));
+
+// Special Routes
 router.post('/ticketRouting', (req, res) => ticketRouting.processWebhook(req.body, res));
 router.post('/closeTicket/:ticketId', (req, res) => closeTicket(req.params, res));
 
