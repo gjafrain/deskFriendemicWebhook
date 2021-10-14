b73061f988dbfa855e10d68b2f5c880556673395
KC Kern <kc@kckern.com>
2021-10-14 13:34:38 +0900

    Payload Processing

--- src/services/Twilio.js
+++ src/services/Twilio.js
@@ -8,14 +8,14 @@ class Twilio {
     constructor() {
     }
     processWebhook(payload, res, SendbirdDesk) { //CUSTOMER TO CLIENT
-        console.log("Twilio Payload",payload);
+        console.log("Twilio Payload",{payload});
         if(!payload) return res.send("No Payload");
-        if(!payload.from) return res.send("No Sender");
+        if(!payload.From) return res.send("No Sender");
         // SEE https://www.twilio.com/docs/usage/webhooks/sms-webhooks
-        const from_phone_num = payload.from.replace(/\D+/g,'');
-        const to_phone_num = payload.to.replace(/\D+/g,'');
+        const message = payload.Body;
+        const from_phone_num = payload.From.replace(/\D+/g,'');
+        const to_phone_num = payload.To.replace(/\D+/g,'');
         const sendbird_id = `twilio_${to_phone_num}_${from_phone_num}`;
-        const message = payload.body;
         const nickname = "SMS " + from_phone_num;
         return SendbirdDesk.processMessage(sendbird_id, message, nickname, { "twilio": to_phone_num.toString(), "customer_phone_num": from_phone_num.toString() }).then(result => res.send(result))
     }
