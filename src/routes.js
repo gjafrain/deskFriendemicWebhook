c7af5cd3b77a5b72d97ce8a057164b3ab18629c6
KC Kern <kc@kckern.com>
2021-10-14 12:12:57 +0900

    Revert "No body in payload"
    This reverts commit a985c370e7cfc2fa526a9f55624840ca0574e04b.

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,7 @@ const GoogleMyBusiness = require('./services/GoogleMyBusiness');
 router.post('/desk',        (req, res) => SendbirdDesk.processWebhook(req.body, res));
 router.post('/chat',        (req, res) => SendbirdChat.processWebhook(req.body, res));
 router.post('/telegram',    (req, res) => Telegram.processWebhook(req.body, res, SendbirdDesk));
-router.post('/twilio',      (req, res) => Twilio.processWebhook(req, res, SendbirdDesk));
+router.post('/twilio',      (req, res) => Twilio.processWebhook(req.body, res, SendbirdDesk));
 router.post('/gmb',         (req, res) => GoogleMyBusiness.processWebhook(req.body, res));
 // Special Routes
 router.get('/desk/closeOpenTickets', (req, res) => SendbirdDesk.closeOpenTickets(res));
