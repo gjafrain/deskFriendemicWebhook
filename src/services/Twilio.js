19a7275f70fcc0153ccbdd7a036bcf5cfde83f1d
gjafrain <gjafrain29@gmail.com>
2021-10-17 16:13:49 +0530

    change appId/token

--- src/services/Twilio.js
+++ src/services/Twilio.js
@@ -1,20 +1,20 @@
 
 const axios = require('axios');
 const SendbirdDesk = require('./SendbirdDesk');
-const accountSid = "AC34a94e0a8b7c7aa1de244a5711718543"; //process.env.TWILIO_ACCOUNT_SID;
-const authToken = '5972495' + "b46c7534" + "2ca9a83096"+ 'acac001';
+const accountSid = "AC5c111a6a9cbaa3cf05e271320a2310f9"; //process.env.TWILIO_ACCOUNT_SID;
+const authToken = "2bedb8fd7" + "cde8fb9" + "102380c" + "79749b9a9" //'5972495' + "b46c7534" + "2ca9a83096"+ 'acac001';
 const twilioClient = require('twilio')(accountSid, authToken);
 class Twilio {
     constructor() {
     }
     processWebhook(payload, res, SendbirdDesk) { //CUSTOMER TO CLIENT
         console.log("Twilio.processWebhook");
-        if(!payload) return res.send("No Payload");
-        if(!payload.From) return res.send("No Sender");
+        if (!payload) return res.send("No Payload");
+        if (!payload.From) return res.send("No Sender");
         // SEE https://www.twilio.com/docs/usage/webhooks/sms-webhooks
         const message = payload.Body;
-        const from_phone_num = payload.From.replace(/\D+/g,'');
-        const to_phone_num = payload.To.replace(/\D+/g,'');
+        const from_phone_num = payload.From.replace(/\D+/g, '');
+        const to_phone_num = payload.To.replace(/\D+/g, '');
         const sendbird_id = `twilio_${to_phone_num}_${from_phone_num}`;
         const nickname = "SMS " + from_phone_num;
         return SendbirdDesk.processMessage(sendbird_id, message, nickname, { "twilio": to_phone_num.toString(), "customer_phone_num": from_phone_num.toString() }).then(result => res.send(result))
@@ -23,11 +23,11 @@ class Twilio {
         twilioClient.messages
             .create({
                 body: message,
-                from: "+"+from_phone_num,
-                to: "+"+to_phone_num
+                from: "+" + from_phone_num,
+                to: "+" + to_phone_num
             })
             .then(message => "SMS Message SENT. " + message)
-            .catch(e=>console.log("SMS FAILED ",e));
+            .catch(e => console.log("SMS FAILED ", e));
         return null
     }
 }
