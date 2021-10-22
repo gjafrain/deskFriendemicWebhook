1e34408a536e7d36cc99c8497e72cc7607cf701c
KC Kern <kc@kckern.com>
2021-10-22 17:29:20 +0900

    Payload console GMB

--- src/services/GoogleMyBusiness.js
+++ src/services/GoogleMyBusiness.js
@@ -5,8 +5,10 @@ class GoogleMyBusiness {
     constructor() {
     }
     processWebhook(payload, res) {
+        console.log("GoogleMyBusiness.processWebhook");
         console.log(payload);
         console.log(JSON.stringify(payload));
+        res.send("GMB Payload: "+JSON.stringify(payload))
         return null
         // SendbirdDesk.processMessage();
     }
