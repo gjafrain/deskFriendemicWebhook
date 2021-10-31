d5e8ebc3c1b145b4d037506abb8d03e7a9bbb32f
gjafrain <gjafrain29@gmail.com>
2021-11-01 00:08:40 +0530

    fix event

--- src/routes.js
+++ src/routes.js
@@ -16,7 +16,7 @@ router.post('/twilio',      (req, res) => Twilio.processWebhook(req.body, res, S
 router.post('/bandwidth',   (req, res) => Bandwidth.processWebhook(req.body, res, SendbirdDesk));
 router.post('/gmb',         (req, res) => GoogleMyBusiness.processWebhook(req.body, res));
 router.post('/facebook',         (req, res) => Facebook.processWebhook(req.body, res));
-router.get('/facebook',         (req, res) => Facebook.verification(req.body, res));
+router.get('/facebook',         (req, res) => Facebook.verification(req, res));
 // Special Routes
 router.get('/desk/closeOpenTickets', (req, res) => SendbirdDesk.closeOpenTickets(res));
 router.get('/desk/closeTicket/:ticketId', (req, res) => SendbirdDesk.closeTicket(req.params.ticketId, res));
