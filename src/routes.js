17151191727504144b89883379fe585a3a76d8e6
gjafrain <gjafrain29@gmail.com>
2021-10-31 23:34:30 +0530

    interation facebook webhook

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ const Bandwidth = require('./services/Bandwidth');
 const Telegram = require('./services/Telegram');
 const Twilio = require('./services/Twilio');
 const GoogleMyBusiness = require('./services/GoogleMyBusiness');
+const Facebook = require('./services/Facebook');
 // Webhook Endpoints
 router.post('/desk',        (req, res) => SendbirdDesk.processWebhook(req.body, res));
 router.post('/chat',        (req, res) => SendbirdChat.processWebhook(req.body, res));
@@ -14,6 +15,8 @@ router.post('/telegram',    (req, res) => Telegram.processWebhook(req.body, res,
 router.post('/twilio',      (req, res) => Twilio.processWebhook(req.body, res, SendbirdDesk));
 router.post('/bandwidth',   (req, res) => Bandwidth.processWebhook(req.body, res, SendbirdDesk));
 router.post('/gmb',         (req, res) => GoogleMyBusiness.processWebhook(req.body, res));
+router.post('/facebook',         (req, res) => Facebook.processWebhook(req.body, res));
+router.get('/facebook',         (req, res) => Facebook.verification(req.body, res));
 // Special Routes
 router.get('/desk/closeOpenTickets', (req, res) => SendbirdDesk.closeOpenTickets(res));
 router.get('/desk/closeTicket/:ticketId', (req, res) => SendbirdDesk.closeTicket(req.params.ticketId, res));
