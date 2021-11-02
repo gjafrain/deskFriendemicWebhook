6b8705a81db54b7552ac547e117a549c26479dcd
KC Kern <kc@kckern.com>
2021-11-02 22:28:09 +0900

    Remove coneole log

--- src/services/GoogleMyBusiness.js
+++ src/services/GoogleMyBusiness.js
@@ -26,7 +26,6 @@ class GoogleMyBusiness {
         const from_name = payload.context.userInfo.displayName.toLowerCase().replace([/[^a-z0-9]/, "-"]);
         const sendbird_id = `google_${conversationId}_${from_name}`;
         const nickname = payload.context.userInfo.displayName;
-        console.log(SendbirdDesk);
         return SendbirdDesk.processMessage(sendbird_id, message, nickname, { "gmb_agent": agent, "conversationId": conversationId }).then(result => res.send(result))
 
     }
